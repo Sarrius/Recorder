2335321eb3acd509c45cfa7bf2ef75dd8be3011d
Shymko <sars267@gmail.com>
2015-12-04 15:22:00 +0200

    добавив таймер і вивів його на activity_main  для зациклювання чи інших потреб

--- app/src/main/java/com/example/sars2/recorder/MainActivity.java
+++ app/src/main/java/com/example/sars2/recorder/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sars2.recorder;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,12 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.*;
 
 public class MainActivity extends AppCompatActivity {
 
+    CountDownTimer countDownTimer = null;
+    TextView display = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +26,37 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //-----------------------INITIALIZATION--------------------------//
+        //----------------настройка інтерфейсу і ресурсів----------------//
+        init(); //конфігурація інтерфейсу
+
 
     }
 
 
     private void testToast(){
         Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void init (){
+        display = (TextView)findViewById(R.id.display);
+
+    }
+    private CountDownTimer createAndStartCoundownTimer () {
+        countDownTimer = new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                display.setText(millisUntilFinished/1000+"");
+            }
+
+            @Override
+            public void onFinish() {
+                display.setText("0");
+            }
+        }.start();
+
+        return countDownTimer;
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
