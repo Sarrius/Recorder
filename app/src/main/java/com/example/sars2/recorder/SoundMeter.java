2d5565acca1daae77457aa22b21afd0716255727
Shymko <sars267@gmail.com>
2015-12-05 12:14:31 +0200

    -виправив measureSound() -добавив вирахування середньої величини showEMALevel()

--- app/src/main/java/com/example/sars2/recorder/SoundMeter.java
+++ app/src/main/java/com/example/sars2/recorder/SoundMeter.java
@@ -1,5 +1,9 @@
 package com.example.sars2.recorder;
 
+import android.media.MediaRecorder;
+
+import java.io.IOException;
+
 import static java.lang.Math.log10;
 
 /**
@@ -7,9 +11,45 @@ import static java.lang.Math.log10;
  */
 public class SoundMeter  {
 
-    public double measureSound( double peakAmplitude, double refference) {
+    private MediaRecorder mediaRecorder = null;
 
-        return 20.0 * log10(peakAmplitude/refference); //взнати актуальність формули
 
+    public  void start(){
+        if (mediaRecorder == null) {
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+            mediaRecorder.setOutputFile("/dev/null");
+            try {
+                mediaRecorder.prepare();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mediaRecorder.start();
+
+        }
+    }
+
+    private double measureSound( ) {
+         if (mediaRecorder!=null)
+        return (mediaRecorder.getMaxAmplitude()/Constants.Audioconfigs.refference); //перевірити практично актуальність даних (поки цифра не зрозуміла)
+        else return 0;
+    }
+
+    public double showEMALevel (){
+        if (measureSound() <= 0){
+            return 0.0;
+        } else {
+            return DynamicData.mEma = Constants.Audioconfigs.FILTER * measureSound() + (1.0 - Constants.Audioconfigs.FILTER) * DynamicData.mEma;
+        }
+    }
+
+    public void stop(){
+        if (mediaRecorder != null){
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+        }
     }
 }
