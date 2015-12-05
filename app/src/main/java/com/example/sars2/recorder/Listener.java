b94490bed724f9fb0cc2717d0715af302507f9b0
Shymko <sars267@gmail.com>
2015-12-05 15:25:26 +0200

    -підключив soundMeter -почистив мусор

--- app/src/main/java/com/example/sars2/recorder/Listener.java
+++ app/src/main/java/com/example/sars2/recorder/Listener.java
@@ -1,77 +1,48 @@
 package com.example.sars2.recorder;
 
 import android.media.MediaRecorder;
-import android.os.Environment;
-
-import java.io.IOException;
-import java.util.Timer;
-
-
-//
-//peakAmplitude = mediarecorder.getMaxAmplitude()
 
 /**
  * Created by sars2 on 04.12.2015.
  */
-public class Listener extends MediaRecorder {
+public class Listener  {
 
     SoundMeter soundMeter = null;
 
 
-    //конструктор налаштовує  таким чином щоб не зберігати файл
-    Listener (){
-
-        this.setAudioSource(MediaRecorder.AudioSource.MIC);
-        this.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        this.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        this.setOutputFile("/dev/null"); //файл не зберігатиметься
-        try {
-            this.prepare();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        soundMeter = new SoundMeter();
+       Listener (){
+             soundMeter = new SoundMeter();
 
     }
 
-    //починає записувати, але не зберігає файл
     public void startToListen (){
-                    this.start();
-
-        if (userIsSpeaking() == true){
-
+        if (soundMeter != null) {
+            soundMeter.start();
         }
+
     }
 
 
-    //тут потрібно відрізнити тишу від голосу
-    //voiceLevel
-    public boolean userIsSpeaking (){
-        while (true){
-            if (soundMeter.measureSound(this.getMaxAmplitude(), Constants.Audioconfigs.refference) >= Constants.Audioconfigs.voiceLevel){
-                return true;
-            } else if (soundMeter.measureSound(this.getMaxAmplitude(), Constants.Audioconfigs.refference) <= Constants.Audioconfigs.voiceLevel){
-                return false;
-            }
+
+    public void stopToListen () {
+        if (soundMeter != null){
+            soundMeter.stop();
+            soundMeter = null;
         }
+
     }
 
-    public void stopListeningAndReset () {
-        this.stop();
-        this.reset();
+
+    public boolean IHearSpeech(){
+
+           if ( soundMeter.showEMALevel() > 1){ //тут потрібно буде експериментальним методом підставляти замість одиниці
+               return true;
+           } else {
+               return false;
+           }
+
+
     }
 
-    //цей метод викликається після того як Listener почув голос і спрацював метод reconfigureSettings
-    public void startRecordingFile(){
-        start();
-    }
 
-    //метод викликається зазвичай після stopListeningAndReset
-
-    public void reconfigureSettings(){
-        this.setAudioSource(AudioSource.MIC);
-        this.setOutputFormat(OutputFormat.THREE_GPP);
-        this.setAudioEncoder(AudioEncoder.AMR_NB);
-        this.setOutputFile(Environment.getExternalStorageDirectory().getAbsolutePath() + FileCaller.nameFile());
-    }
 }
